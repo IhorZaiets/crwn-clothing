e54c2127ea631e8651c7059e671d2dd207641b8f
Ihor Zaiets <zaecziam@gmail.com>
2022-03-21 00:21:36 +0200

    added navigation to menu-items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
+
+import { Route, Routes } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component';
+
+
+
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>
+        Hats page
+      </h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+          <Route exact path = '/' element={<HomePage />} />
+          <Route path = '/shop/hats' element={<HatsPage />} />
+      </Routes>
     </div>
-    
   );
 }
 
